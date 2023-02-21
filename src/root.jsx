48e40754a7872a858b116835240e8681af19ea78
Steve Della Valentina <s.dellavalentina@gmail.com>
2023-02-21 11:36:05 -0700

    Update root styles

--- src/root.jsx
+++ src/root.jsx
@@ -22,11 +22,18 @@ export default function Root() {
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta name="description" content={metadata.description} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet" />
       </Head>
       <Body>
         <ErrorBoundary>
-          <A href="/">Sophia Wilson</A>
-          <A href="/about">About</A>
+          <nav>
+            <h1>
+              <A href="/">Sophia Wilson</A>
+            </h1>
+            <A href="/about">/about</A>
+          </nav>
           <Suspense>
             <main>
               <Routes>
