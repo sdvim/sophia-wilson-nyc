0d45079e5a45d6f090f743baf71357e70f752a3b
Steve Della Valentina <s.dellavalentina@gmail.com>
2023-02-20 18:32:22 -0700

    Add images

---
+++ src/data/carousel-items.js
@@ -0,0 +1,191 @@
+export default [
+  {
+    filename: '1_8.jpg',
+  },
+  {
+    filename: 'ARCHIVE-CHIVES_Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'BLU-11_20_Prints_3.jpg',
+  },
+  {
+    filename: 'CITYKITS_Press-Culture_14.jpg',
+  },
+  {
+    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_3.jpg',
+  },
+  {
+    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_7.jpg',
+  },
+  {
+    filename: 'DIANA-VERAS-TEST_Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'Dr. Seuss 10.jpg',
+  },
+  {
+    filename: 'Dr. Seuss 2.jpg',
+  },
+  {
+    filename: 'Dr. Seuss 7.jpg',
+  },
+  {
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_1.jpg',
+  },
+  {
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_34.jpg',
+  },
+  {
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_43.jpg',
+  },
+  {
+    filename: 'EVAN-MOCK-MET-GALLA_Final-Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'GOOGLE-PIXEL-MAGIC-ERASER_Final-Hi-Res_1.jpg',
+  },
+  {
+    filename: 'GOOGLE-PIXEL-MAGIC-ERASER_Licensed-Work_4.jpg',
+  },
+  {
+    filename: 'GRAMMYS-2022_Finals-Edited_16.jpg',
+  },
+  {
+    filename: 'IG-NFT-FINAL-EDITED_3.jpg',
+  },
+  {
+    filename: 'INAMORATA-SILK_Edited-Selects_35.jpg',
+  },
+  {
+    filename: 'INSTA-SAD-GIRLS-POPSUGAR_Hi-Res-Edited_6.jpg',
+  },
+  {
+    filename: 'IOWA_Edited_3.jpg',
+  },
+  {
+    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_3.jpg',
+  },
+  {
+    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_5.jpg',
+  },
+  {
+    filename: 'Jaden and Willow Smith for Teen Vogue.jpg',
+  },
+  {
+    filename: 'LOLA-PICAYO-11_20_2.jpg',
+  },
+  {
+    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_3.jpg',
+  },
+  {
+    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_4.jpg',
+  },
+  {
+    filename: 'Milk 1.jpg',
+  },
+  {
+    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_2.jpg',
+  },
+  {
+    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_3.jpg',
+  },
+  {
+    filename: 'NIKEWOMXN_Nike-Finals-Images_1.jpg',
+  },
+  {
+    filename: 'NYLON-JAPAN_2.jpg',
+  },
+  {
+    filename: 'NYMAG-RENEE-GOLDSBERRY_Final-Edited_1.jpg',
+  },
+  {
+    filename: 'OUI-THE-PEOPLE_Edited-Prints_23.jpg',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_10.jpg',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_2.jpg',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_5.jpg',
+  },
+  {
+    filename: 'PARADE-RIB_Edited_7.jpg',
+  },
+  {
+    filename: 'PARADE-SUPERSOFT_Edited_10.jpg',
+  },
+  {
+    filename: 'PARADE-SUPERSOFT_Edited_7.jpg',
+  },
+  {
+    filename: 'REFINERY29-STREETSTYLE_Edited-Red_69.jpg',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_1.jpg',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_2.jpg',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_3.jpg',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_5.jpg',
+  },
+  {
+    filename: 'SLASHED-TIA-BULLETT_Amanda_7.jpg',
+  },
+  {
+    filename: 'SLASHEDTIABEACH_Lo-res_1.jpg',
+  },
+  {
+    filename: 'STUDS-NYC_Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'STUDS-NYC_Edited-Selects_4.jpg',
+  },
+  {
+    filename: 'Schön! 2.jpg',
+  },
+  {
+    filename: 'TEEN-VOGUE-SEPTEMBER-2019-FENTY_Edited-Selects_5.jpg',
+  },
+  {
+    filename: 'THE-CUT-2018_1.jpg',
+  },
+  {
+    filename: 'THE-CUT-2018_2.jpg',
+  },
+  {
+    filename: 'THE-CUT-2018_4.jpg',
+  },
+  {
+    filename: 'TIA-ADEOLA-MANSION-VIDEO-SHOOT_Edited-Selects_1.jpg',
+  },
+  {
+    filename: 'TIA-HULU_Hi-Res-Edited_7.jpg',
+  },
+  {
+    filename: 'VANS-OC-2018_2.jpg',
+  },
+  {
+    filename: 'VANS-OC-2018_5.jpg',
+  },
+  {
+    filename: 'VOGUE-ITALIA-2018_3.jpg',
+  },
+  {
+    filename: 'YASMIN-AT-THE-JANE_Edited_3.jpg',
+  },
+  {
+    filename: 'r01=Rich_Brian_for_Guess_X_88rising-1.jpg',
+  },
+  {
+    filename: 'sophiasaad7.jpg',
+  },
+];
