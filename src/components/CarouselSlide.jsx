fef41318e67159364034613b55ebcda80f5d4207
Steve Della Valentina <s.dellavalentina@gmail.com>
2023-03-01 00:32:10 -0700

    Add srcset attribute

--- src/components/CarouselSlide.jsx
+++ src/components/CarouselSlide.jsx
@@ -1,12 +1,14 @@
 import './CarouselSlide.css';
 
 const CarouselSlide = (props) => {
+  const img = `/images/${encodeURIComponent(props.filename)}?nf_resize=fit&h=`;
   return (
     <div classList={{'is-active': props.active, 'carousel-slide': true }}>
       <img
         class="carousel-slide-image"
-        style={{ 'background-image': `url(/images/${encodeURIComponent(props.filename)}?nf_resize=fit&h=30)` }}
-        src={`/images/${encodeURIComponent(props.filename)}?nf_resize=fit&h=720`}
+        style={{ 'background-image': `url(${img}30)` }}
+        srcset={`${img}480, ${img}720 1.5x, ${img}1440 2x`}
+        src={`${img}720`}
       />
       <h4 class="carousel-slide-title">{props.label || ' '}</h4>
     </div>
