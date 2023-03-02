639bda699382fb530777e94a124b2b2df1d7ff0a
Steve Della Valentina <s.dellavalentina@gmail.com>
2023-03-01 17:48:46 -0700

    Refactor slide image to LazyImage

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,19 +1,11 @@
+import LazyImage from './LazyImage';
 import './Slide.css';
 
 const Slide = (props) => {
-  const img = `/images/${encodeURIComponent(props.filename)}?nf_resize=fit&h=`;
   return (
-    <div
-      classList={{'is-active': props.active, 'slide': true }}
-      style={{ 'background-image': `url(${img}64)` }}
-    >
-      <img
-        class="slide-image"
-        loading="lazy"
-        src={`${img}916`}
-        srcset={`${img}640 640w, ${img}916 916w, ${img}1030 1030w`}
-      />
-      <h4>{props.label || ' '}</h4>
+    <div classList={{'is-active': props.active, 'slide': true }}>
+      <LazyImage src={props.filename} />
+      <h4>{props.label}</h4>
     </div>
   );
 }
