e20c884a21acdd6e96fa19d6cd8923adc1b74988
Steve Della Valentina <s.dellavalentina@gmail.com>
2023-03-01 17:51:56 -0700

    Clean up debugging

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -1,11 +1,10 @@
 import './LazyImage.css';
 
 const LazyImage = (props) => {
-  const img = `https://sophiawilson.netlify.app/images/${encodeURIComponent(props.src)}?nf_resize=fit`;
+  const img = `/images/${encodeURIComponent(props.src)}?nf_resize=fit`;
   
   return (
     <div class="lazy-image" style={{ 'background-image': `url(${img}&h=64)` }}>
-      {/* <img src={`${img}&h=64`} aria-hidden="true" /> */}
       <img src={`${img}&h=916`} loading="lazy" />
     </div>
   );
