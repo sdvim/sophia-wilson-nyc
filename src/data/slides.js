b28812db8052f23beefbe6e93fda2a6f27c2c3a2
Steve Della Valentina <stevedev@google.com>
2023-03-07 18:22:53 -0800

    Reorder slides

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,246 +1,114 @@
 export default [
   {
-    filename: '1_8.jpg',
-    label: 'Timberland',
-  },
-  {
-    filename: 'ARCHIVE-CHIVES_Edited-Selects_1.jpg',
-    label: '',
-  },
-  {
-    filename: 'BLU-11_20_Prints_3.jpg',
-    label: '',
-  },
-  {
-    filename: 'CITYKITS_Press-Culture_14.jpg',
-    label: '',
-  },
-  {
-    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_3.jpg',
-    label: 'Danai Gurira for Vogue',
-  },
-  {
-    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_7.jpg',
-    label: 'Danai Gurira for Vogue',
-  },
-  {
-    filename: 'DIANA-VERAS-TEST_Edited-Selects_1.jpg',
-    label: 'Danai Gurira for Vogue',
-  },
-  {
-    filename: 'Dr. Seuss 10.jpg',
-    label: '',
-  },
-  {
-    filename: 'Dr. Seuss 2.jpg',
-    label: '',
-  },
-  {
-    filename: 'Dr. Seuss 7.jpg',
-    label: '',
-  },
-  {
-    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_1.jpg',
-    label: 'Emily Ratajkowski & Kaia Gerber for Vogue Magazine',
-  },
-  {
-    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_34.jpg',
-    label: 'Kaia Gerber for Vogue Magazine',
-  },
-  {
-    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_43.jpg',
-    label: 'Vogue Magazine',
-  },
-  {
-    filename: 'EVAN-MOCK-MET-GALLA_Final-Edited-Selects_1.jpg',
-    label: 'Evan Mock for Office Magazine',
+    filename: 'NYLON-JAPAN_2.jpg',
+    label: 'Nylon Japan',
   },
   {
     filename: 'GOOGLE-PIXEL-MAGIC-ERASER_Final-Hi-Res_1.jpg',
     label: 'Google',
   },
   {
-    filename: 'GOOGLE-PIXEL-MAGIC-ERASER_Licensed-Work_4.jpg',
-    label: '',
-  },
-  {
-    filename: 'GRAMMYS-2022_Finals-Edited_16.jpg',
-    label: 'Petra Collins for i-D',
-  },
-  {
     filename: 'IG-NFT-FINAL-EDITED_3.jpg',
     label: 'Meta',
   },
   {
-    filename: 'INAMORATA-SILK_Edited-Selects_35.jpg',
-    label: 'Inamorata',
-  },
-  {
-    filename: 'INSTA-SAD-GIRLS-POPSUGAR_Hi-Res-Edited_6.jpg',
-    label: 'Instagram',
-  },
-  {
-    filename: 'IOWA_Edited_3.jpg',
-    label: '',
-  },
-  {
-    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_1.jpg',
-    label: 'Office Magazine',
-  },
-  {
-    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_3.jpg',
-    label: 'Office Magazine',
-  },
-  {
     filename: 'JAMONT-PERSONAL_Final-Edited-Selects_5.jpg',
-    label: '',
-  },
-  {
-    filename: 'Jaden and Willow Smith for Teen Vogue.jpg',
-    label: 'Jaden and Willow Smith for Teen Vogue',
-  },
-  {
-    filename: 'LOLA-PICAYO-11_20_2.jpg',
-    label: '',
-  },
-  {
-    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_3.jpg',
-    label: '',
-  },
-  {
-    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_4.jpg',
-    label: '',
-  },
-  {
-    filename: 'Milk 1.jpg',
-    label: 'Milk',
-  },
-  {
-    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_2.jpg',
-    label: 'Sue Bird & Megan Rapinoe for Nike',
-  },
-  {
-    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_3.jpg',
-    label: 'Sue Bird & Megan Rapinoe for Nike',
-  },
-  {
-    filename: 'NIKEWOMXN_Nike-Finals-Images_1.jpg',
-    label: 'Nike',
-  },
-  {
-    filename: 'NYLON-JAPAN_2.jpg',
-    label: 'Nylon Japan',
-  },
-  {
-    filename: 'NYMAG-RENEE-GOLDSBERRY_Final-Edited_1.jpg',
-    label: 'Renee Goldsberry for New York Magazine',
-  },
-  {
-    filename: 'OUI-THE-PEOPLE_Edited-Prints_23.jpg',
-    label: 'Oui The People',
-  },
-  {
-    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_10.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_2.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_5.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'PARADE-RIB_Edited_7.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'PARADE-SUPERSOFT_Edited_10.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'PARADE-SUPERSOFT_Edited_7.jpg',
-    label: 'Parade',
-  },
-  {
-    filename: 'REFINERY29-STREETSTYLE_Edited-Red_69.jpg',
-    label: 'Refinery29',
-  },
-  {
-    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_1.jpg',
-    label: '',
-  },
-  {
-    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_2.jpg',
-    label: '',
-  },
-  {
-    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_3.jpg',
-    label: '',
+    label: 'Office Magazine',
   },
   {
     filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_5.jpg',
     label: '',
   },
   {
-    filename: 'SLASHED-TIA-BULLETT_Amanda_7.jpg',
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_43.jpg',
+    label: 'Vogue Magazine',
+  },
+  {
+    filename: 'Dr. Seuss 2.jpg',
     label: '',
   },
   {
-    filename: 'SLASHEDTIABEACH_Lo-res_1.jpg',
+    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_2.jpg',
+    label: 'Sue Bird & Megan Rapinoe for Nike',
+  },
+  {
+    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_7.jpg',
+    label: 'Danai Gurira for Vogue',
+  },
+  {
+    filename: '1_8.jpg',
+    label: 'Timberland',
+  },
+  {
+    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_4.jpg',
     label: '',
   },
   {
-    filename: 'STUDS-NYC_Edited-Selects_1.jpg',
-    label: 'Studs',
+    filename: 'EVAN-MOCK-MET-GALLA_Final-Edited-Selects_1.jpg',
+    label: 'Evan Mock for Office Magazine',
   },
   {
-    filename: 'STUDS-NYC_Edited-Selects_4.jpg',
-    label: 'Studs',
+    filename: 'INSTA-SAD-GIRLS-POPSUGAR_Hi-Res-Edited_6.jpg',
+    label: 'Instagram',
   },
   {
-    filename: 'Schon 2.jpg',
+    filename: 'DIANA-VERAS-TEST_Edited-Selects_1.jpg',
+    label: 'Danai Gurira for Vogue',
+  },
+  {
+    filename: 'NIKEWOMXN_Nike-Finals-Images_1.jpg',
+    label: 'Nike',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_2.jpg',
+    label: 'Parade',
+  },
+  {
+    filename: 'GRAMMYS-2022_Finals-Edited_16.jpg',
+    label: 'Petra Collins for i-D',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_3.jpg',
     label: '',
   },
   {
-    filename: 'TEEN-VOGUE-SEPTEMBER-2019-FENTY_Edited-Selects_5.jpg',
-    label: 'Fenty x Teen Vogue',
+    filename: 'PARADE-SUPERSOFT_Edited_7.jpg',
+    label: 'Parade',
+  },
+  {
+    filename: 'Dr. Seuss 7.jpg',
+    label: '',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_5.jpg',
+    label: 'Parade',
+  },
+  {
+    filename: 'sophiasaad7.jpg',
+    label: 'New York Times Magazine',
+  },
+  {
+    filename: 'DANAI-VOGUE-MET-GALA_Final-Edited-Selects_3.jpg',
+    label: 'Danai Gurira for Vogue',
+  },
+  {
+    filename: 'PARADE-RIB_Edited_7.jpg',
+    label: 'Parade',
   },
   {
     filename: 'THE-CUT-2018_1.jpg',
     label: 'New York Magazine / The Cut',
   },
   {
-    filename: 'THE-CUT-2018_2.jpg',
-    label: 'New York Magazine / The Cut',
+    filename: 'PARADE-SUPERSOFT_Edited_10.jpg',
+    label: 'Parade',
   },
   {
-    filename: 'THE-CUT-2018_4.jpg',
-    label: 'New York Magazine / The Cut',
-  },
-  {
-    filename: 'TIA-ADEOLA-MANSION-VIDEO-SHOOT_Edited-Selects_1.jpg',
+    filename: 'IOWA_Edited_3.jpg',
     label: '',
   },
   {
-    filename: 'TIA-HULU_Hi-Res-Edited_7.jpg',
-    label: '',
-  },
-  {
-    filename: 'VANS-OC-2018_2.jpg',
-    label: 'Opening Ceremony',
-  },
-  {
-    filename: 'VANS-OC-2018_5.jpg',
-    label: 'Opening Ceremony',
-  },
-  {
-    filename: 'VOGUE-ITALIA-2018_3.jpg',
-    label: 'Vogue Italia',
-  },
-  {
-    filename: 'YASMIN-AT-THE-JANE_Edited_3.jpg',
+    filename: 'Dr. Seuss 10.jpg',
     label: '',
   },
   {
@@ -248,7 +116,135 @@ export default [
     label: '88rising x Guess',
   },
   {
-    filename: 'sophiasaad7.jpg',
-    label: 'New York Times Magazine',
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_1.jpg',
+    label: 'Emily Ratajkowski & Kaia Gerber for Vogue Magazine',
+  },
+  {
+    filename: 'SLASHEDTIABEACH_Lo-res_1.jpg',
+    label: '',
+  },
+  {
+    filename: 'Jaden and Willow Smith for Teen Vogue.jpg',
+    label: 'Jaden and Willow Smith for Teen Vogue',
+  },
+  {
+    filename: 'TIA-ADEOLA-MANSION-VIDEO-SHOOT_Edited-Selects_1.jpg',
+    label: '',
+  },
+  {
+    filename: 'STUDS-NYC_Edited-Selects_4.jpg',
+    label: 'Studs',
+  },
+  {
+    filename: 'EMILY-RATAJKOWSKI-VOGUE_Emily-Edited-Film-Selects_34.jpg',
+    label: 'Kaia Gerber for Vogue Magazine',
+  },
+  {
+    filename: 'VANS-OC-2018_2.jpg',
+    label: 'Opening Ceremony',
+  },
+  {
+    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_3.jpg',
+    label: 'Office Magazine',
+  },
+  {
+    filename: 'CITYKITS_Press-Culture_14.jpg',
+    label: '',
+  },
+  {
+    filename: 'OUI-THE-PEOPLE_Edited-Prints_23.jpg',
+    label: 'Oui The People',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_2.jpg',
+    label: '',
+  },
+  {
+    filename: 'JAMONT-PERSONAL_Final-Edited-Selects_1.jpg',
+    label: 'Office Magazine',
+  },
+  {
+    filename: 'Schön! 2.jpg',
+    label: '',
+  },
+  {
+    filename: 'NYMAG-RENEE-GOLDSBERRY_Final-Edited_1.jpg',
+    label: 'Renee Goldsberry for New York Magazine',
+  },
+  {
+    filename: 'VOGUE-ITALIA-2018_3.jpg',
+    label: 'Vogue Italia',
+  },
+  {
+    filename: 'VANS-OC-2018_5.jpg',
+    label: 'Opening Ceremony',
+  },
+  {
+    filename: 'YASMIN-AT-THE-JANE_Edited_3.jpg',
+    label: '',
+  },
+  {
+    filename: 'LOLA-PICAYO-11_20_2.jpg',
+    label: '',
+  },
+  {
+    filename: 'SALEM-MITCHELL-LA-TEST_Final-Edit_1.jpg',
+    label: '',
+  },
+  {
+    filename: 'STUDS-NYC_Edited-Selects_1.jpg',
+    label: 'Studs',
+  },
+  {
+    filename: 'REFINERY29-STREETSTYLE_Edited-Red_69.jpg',
+    label: 'Refinery29',
+  },
+  {
+    filename: 'MAE-MCKAGAN-LA-TEST_Contact-Sheets-Edited-Selects_3.jpg',
+    label: '',
+  },
+  {
+    filename: 'INAMORATA-SILK_Edited-Selects_35.jpg',
+    label: 'Inamorata',
+  },
+  {
+    filename: 'THE-CUT-2018_4.jpg',
+    label: 'New York Magazine / The Cut',
+  },
+  {
+    filename: 'Milk 1.jpg',
+    label: 'Milk',
+  },
+  {
+    filename: 'ARCHIVE-CHIVES_Edited-Selects_1.jpg',
+    label: '',
+  },
+  {
+    filename: 'NIKE-SUE-BIRD-MEGAN-RAPINOE_Final-Edit_3.jpg',
+    label: 'Sue Bird & Megan Rapinoe for Nike',
+  },
+  {
+    filename: 'THE-CUT-2018_2.jpg',
+    label: 'New York Magazine / The Cut',
+  },
+  {
+    filename: 'BLU-11_20_Prints_3.jpg',
+    label: '',
+  },
+  {
+    filename: 'TEEN-VOGUE-SEPTEMBER-2019-FENTY_Edited-Selects_5.jpg',
+    label: 'Fenty x Teen Vogue',
+  },
+  {
+    filename: 'TIA-HULU_Hi-Res-Edited_7.jpg',
+    label: '',
+  },
+  {
+    filename: 'PARADE-BLACK-HISTORY-MONTH_Lo-res-Edited_10.jpg',
+    label: 'Parade',
+  },
+  {
+    filename: 'SLASHED-TIA-BULLETT_Amanda_7.jpg',
+    label: '',
   },
 ];
